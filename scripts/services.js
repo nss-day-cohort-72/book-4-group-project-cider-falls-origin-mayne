bb7fa7397a0b8df709b6b0d9954119597aef15ff
Ben Merlotti <benmerlotti@gmail.com>
2024-08-19 09:22:46 -0500

    services event listener

--- scripts/services.js
+++ scripts/services.js
@@ -1,14 +1,31 @@
-import { getServices} from "./database.js"
+import { getServices, getParkAreaServices, getAreas} from "./database.js"
 
 const services = getServices()
+const parkAreaServices = getParkAreaServices()
+const areas = getAreas()
 
 export const Services = () => {
     let html = "<ul>"
     for (const service of services) {
         html += `
-            <li>${service.name}</li>
+            <li data-id="${service.id}" data-type="service">${service.name}</li>
     `
     }
     html = html + "</ul>"
     return html;
 };
+
+
+
+document.addEventListener(
+    "click", (clickEvent) => {
+        const whatWasClickedOn = clickEvent.target
+
+        if (whatWasClickedOn.dataset.type === "service") {
+            const serviceId = parseInt(whatWasClickedOn.dataset.id)
+            const matchedParkAreaServices = parkAreaServices.filter(service => service.serviceId === serviceId)
+            console.log(matchedParkAreaServices)
+
+        }
+     }
+)
