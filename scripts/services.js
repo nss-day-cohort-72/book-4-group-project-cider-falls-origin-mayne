a0d093685db6112237d4cb627678ab1fa6b43054
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-08-19 10:08:13 -0500

    Add matched areas function

--- scripts/services.js
+++ scripts/services.js
@@ -1,31 +1,56 @@
-import { getServices, getParkAreaServices, getAreas} from "./database.js"
+import { getServices, getParkAreaServices, getAreas } from './database.js';
 
-const services = getServices()
-const parkAreaServices = getParkAreaServices()
-const areas = getAreas()
+const services = getServices();
+const parkAreaServices = getParkAreaServices();
+const areas = getAreas();
 
 export const Services = () => {
-    let html = "<ul>"
-    for (const service of services) {
-        html += `
+  let html = '<ul>';
+  for (const service of services) {
+    html += `
             <li data-id="${service.id}" data-type="service">${service.name}</li>
-    `
-    }
-    html = html + "</ul>"
-    return html;
+    `;
+  }
+  html = html + '</ul>';
+  return html;
 };
 
+document.addEventListener('click', (clickEvent) => {
+  const whatWasClickedOn = clickEvent.target;
 
+  if (whatWasClickedOn.dataset.type === 'service') {
+    const serviceId = parseInt(whatWasClickedOn.dataset.id);
+    const matchedParkAreaServices = parkAreaServices.filter(
+      (service) => service.serviceId === serviceId
+    );
+    const mainAttractionArr = [];
+    for (const parkArea of matchedParkAreaServices) {
+      mainAttractionArr.push(findMatchedArea(parkArea, areas));
+    }
 
-document.addEventListener(
-    "click", (clickEvent) => {
-        const whatWasClickedOn = clickEvent.target
+    const mainAttractions = mainAttractionArr
+      .map((attraction) => attraction.mainAttraction)
+      
+      if (mainAttractions.length === 2) {
+        
+      }
+      
 
-        if (whatWasClickedOn.dataset.type === "service") {
-            const serviceId = parseInt(whatWasClickedOn.dataset.id)
-            const matchedParkAreaServices = parkAreaServices.filter(service => service.serviceId === serviceId)
-            console.log(matchedParkAreaServices)
+    const serviceName = whatWasClickedOn.textContent;
+    if (mainAttractions === '') {
+      window.alert(`${serviceName} is not offered in any location`);
+    } else {
+      window.alert(`${serviceName} is available in ${mainAttractions}`);
+    }
+  }
+});
 
-        }
-     }
-)
+const findMatchedArea = (parkArea, areaArr) => {
+  let matchedArea = null;
+  for (const area of areaArr) {
+    if (area.id === parkArea.areaId) {
+      matchedArea = area;
+    }
+  }
+  return matchedArea;
+};
